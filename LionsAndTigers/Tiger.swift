2cf7a78861c617821866a08f5642f33ec1962adf
Eliot Arntz <arntzel@gmail.com>
2014-07-09 15:13:48 -0400

    Using_the_Tiger_Instance

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,19 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Eliot Arntz on 7/9/14.
+//  Copyright (c) 2014 Bitfountain.io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+ 
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    
+    @IBOutlet var myImageView: UIImageView
+    
+    @IBOutlet var nameLabel: UILabel
+    
+    @IBOutlet var ageLabel: UILabel
+    
+    @IBOutlet var breedLabel: UILabel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named:"BengalTiger.jpg")
+        
+        println("My Tiger's name is: \(myTiger.name) and its' breed is \(myTiger.breed) and the image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+    }
 
 }
 

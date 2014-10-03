7a7f30fff32b9179f8dee1f89754d20027631872
Daniel Spector <danielyspector@gmail.com>
2014-10-03 10:08:09 -0400

    commit

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -18,11 +18,8 @@ class ViewController: UIViewController {
     @IBOutlet var ageLabel: UILabel!
     
     @IBOutlet var breedLabel: UILabel!
-    
-    @IBOutlet var randomFactLabel: UILabel!
-    
+
     var myTigers: [Tiger] = []
-    
     var currentIndex = 0
     
     override func viewDidLoad() {
@@ -35,22 +32,14 @@ class ViewController: UIViewController {
         myTiger.age = 3
         myTiger.image = UIImage(named:"BengalTiger.jpg")
         
-//        myTiger.chuff()
-        myTiger.chuffANumberOfTimes(3)
-        myTiger.chuffANumberOfTimes(5, isLoud: false)
-        myTiger.age = myTiger.ageInTigerYearsFromAge(myTiger.age)
-        
         self.myTigers.append(myTiger)
         
         println("ViewController: My Tiger's name is: \(myTiger.name) and its' breed is \(myTiger.breed) and the image is \(myTiger.image)")
         
-        self.myImageView.image = myTiger.image
-        self.nameLabel.text = myTiger.name
-        self.ageLabel.text = "\(myTiger.age)"
-        self.breedLabel.text = myTiger.breed
-        self.randomFactLabel.text = myTiger.randomFact()
-        
-        self.currentIndex = 0
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
         
         var secondTiger = Tiger()
         secondTiger.name = "Tigress"
@@ -58,28 +47,17 @@ class ViewController: UIViewController {
         secondTiger.age = 2
         secondTiger.image = UIImage(named:"IndochineseTiger.jpg")
         
-        secondTiger.age = secondTiger.ageInTigerYearsFromAge(secondTiger.age)
-        
         var thirdTiger = Tiger()
         thirdTiger.name = "Jacob"
         thirdTiger.breed = "Malayan Tiger"
         thirdTiger.age = 4
         thirdTiger.image = UIImage(named:"MalayanTiger.jpg")
         
-        thirdTiger.age = thirdTiger.ageInTigerYearsFromAge(thirdTiger.age)
-        
         var fourthTiger = Tiger()
         fourthTiger.name = "Spar"
         fourthTiger.breed = "Siberian Tiger"
         fourthTiger.age = 5
         fourthTiger.image = UIImage(named:"SiberianTiger.jpg")
-        
-        fourthTiger.age = fourthTiger.ageInTigerYearsFromAge(fourthTiger.age)
-        
-        secondTiger.chuffANumberOfTimes(2)
-//        secondTiger.chuff()
-//        thirdTiger.chuff()
-//        fourthTiger.chuff()
         
         self.myTigers += [secondTiger, thirdTiger, fourthTiger]
     }
@@ -90,30 +68,32 @@ class ViewController: UIViewController {
     }
 
     @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
-        
-        println("\(sender)")
-        
         var randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
         
-        while self.currentIndex == randomIndex {
+        while randomIndex == currentIndex {
             randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
         }
         
-        self.currentIndex = randomIndex
+        currentIndex = randomIndex
         
-        let tiger = self.myTigers[randomIndex]
+        let tiger = myTigers[randomIndex]
         
-        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
-            
-            self.myImageView.image = tiger.image
-            self.nameLabel.text = tiger.name
-            self.ageLabel.text = "\(tiger.age)"
-            self.breedLabel.text = tiger.breed
-            self.randomFactLabel.text = tiger.randomFact()
-            
-            }, completion: { (finished: Bool) -> () in
-                
-            })
+        myImageView.image = tiger.image
+        nameLabel.text = tiger.name
+        breedLabel.text = tiger.breed
+        ageLabel.text = "\(tiger.age)"
+        
+        UIView.transitionWithView(self.view, duration: 0.5, options: UIViewAnimationOptions.TransitionCurlUp,
+            animations: {
+                self.myImageView.image = tiger.image
+                self.nameLabel.text = tiger.name
+                self.breedLabel.text = tiger.breed
+                self.ageLabel.text = "\(tiger.age)"
+
+            }, completion: {
+                (finished: Bool) -> () in
+        })
+        
     }
 }
 
